501062348d1623c4b1137d4b0bc2815bd3028f45
EricMontez <eric.alexander.montez@gmail.com>
2026-03-25 15:52:15 -0700

    A55 First Attempt

--- main.cpp
+++ main.cpp
@@ -1,31 +1,24 @@
-
 #include <iostream>
-#include <string>
 using namespace std;
-
 int main()
 {
-	int N; // the number of students;
-	int M; // the number of scores;
+  int N, M;
+  cout << "Number of Students and Number of Subjects: ";
+  cin >> N >> M;
+  string sname;
+  int score, total = 0;
 
-	cout << "Enter the number of students: " ;
-	cin >> N;
-	cout << "Enter the number of scores for each student: " ;
-	cin >> M;
-
-	// TODO: declare variables for student name, score, sum, average
-
-	for(int i=0; i<N; i++)
-	{
-		// TODO: read student name and initialize sum
-
-		for (int j=0; j<M; j++)
-		{
-			// TODO: read each score and accumulate sum
-		}
-
-		// TODO: calculate average and print name, sum, average
-	}
-
-	return 0;
+  for (int i=0; i<N; i++)
+  {
+    cout << "Enter Student name: ";
+    cin >> sname;  
+    for(int j=0; j<M; j++)
+    {
+      cout << "Enter Score: ";
+      cin >> score;
+      total += score;    
+    }
+    double avg = total / (double)M;
+    cout << sname << "\t" << total << "\t" << avg << endl;
+  }
 }
